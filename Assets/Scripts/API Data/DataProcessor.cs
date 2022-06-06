8e6a78eb29bfd1de61a4ca19a9d290f50e5b781a
Ertuğrul Baran <ert.baran@gmail.com>
2022-06-06 23:47:41 +0300

    Get Api Data Set

--- Assets/Scripts/API Data/DataProcessor.cs
+++ Assets/Scripts/API Data/DataProcessor.cs
@@ -1,12 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class DataProcessor : MonoBehaviour
 {
-    public void GetTimeDate()
+    [System.Obsolete]
+    private void Awake()
     {
-        TimeData timeData = APIHelper.GetDayLight();
-        Debug.Log(timeData.isDayLightSavingsTime);
+        SendTimeRequest();
+    }
+
+    [System.Obsolete]
+    public void SendTimeRequest()
+    {
+        StartCoroutine(GetTime());
+    }
+
+    [System.Obsolete]
+    IEnumerator GetTime()
+    {
+        string url = "http://worldclockapi.com/api/jsonp/cet/now?callback=mycallback";
+        using (UnityWebRequest request = UnityWebRequest.Get(url))
+        {
+            yield return request.SendWebRequest();
+            if (request.isNetworkError || request.isHttpError)
+            {
+                Debug.Log(request.error);
+            }
+            else
+            {
+                string sonText = request.downloadHandler.text;
+                Debug.Log(sonText);
+            }
+        } ;
     }
 }
--- Assets/Scripts/API Data/TimeData.cs
+++ Assets/Scripts/API Data/TimeData.cs
@@ -4,5 +4,5 @@ using UnityEngine;
 
 public class TimeData
 {
-    public string isDayLightSavingsTime;
+    public bool isDayLightSavingsTime;
 }
